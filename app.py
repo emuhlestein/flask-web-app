969e3c7e1826f12a134d9c1cec5867ab51c2f355
Ed Muhlestein <emuhlestein@yahoo.com>
2025-11-21 15:44:29 -0700

    Add about route (#1)

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def home():
     return "Hello, Flask with Docker!"
 
+@app.route('/about')
+def about():
+    return "<p>The About page</p>"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
