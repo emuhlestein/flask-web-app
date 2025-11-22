12dc1761d2e03710cbba9ddfe4ddcb6694dcf6f4
Ed Muhlestein <emuhlestein@yahoo.com>
2025-11-22 12:53:58 -0700

    Add name route (#2)

--- app.py
+++ app.py
@@ -12,6 +12,10 @@ def home():
 def about():
     return "<p>The About page</p>"
 
+@app.route('/name/<name>')
+def name(name):
+    return f'<h3>Hello {name}</h3>'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
